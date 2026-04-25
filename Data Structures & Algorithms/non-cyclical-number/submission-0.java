15fad18b9daef88af9263578d319178024b375cf
Rehman-12-acer <rehmanacer999@gmail.com>
2026-04-25 21:02:48 +0530

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> visit= new HashSet<>();
+
+        while(!visit.contains(n))
+        {
+            visit.add(n);
+            n=sumofsquares(n);
+            if(n==1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private int sumofsquares(int n)
+    {
+        int output=0;
+
+        while(n>0)
+        {
+            int digit=n%10;
+            digit=digit*digit;
+            output+=digit;
+            n/=10;
+        }
+        return output;
+    }
+}
