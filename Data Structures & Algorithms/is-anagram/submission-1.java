89b9d62b3f1dafe1efab3e6b5ffd55106d3180e6
Rehman-12-acer <rehmanacer999@gmail.com>
2026-07-18 20:23:23 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> mp1= new HashMap<>();
+        HashMap<Character,Integer> mp2= new HashMap<>();
+
+        for(char c:s.toCharArray())
+        {
+            mp1.put(c,mp1.getOrDefault(c,0)+1);
+        }
+        for(char c:t.toCharArray())
+        {
+            mp2.put(c,mp2.getOrDefault(c,0)+1);
+        }
+        return mp1.equals(mp2);
+    }
+}
