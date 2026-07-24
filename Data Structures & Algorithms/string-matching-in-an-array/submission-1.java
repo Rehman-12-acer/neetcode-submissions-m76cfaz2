ffadb15dd06ed88d62b28a0b74e6d5adc5b9f7bd
Rehman-12-acer <rehmanacer999@gmail.com>
2026-07-24 19:00:24 +0530

    Add: string-matching-in-an-array - submission-1

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> res= new ArrayList<>();
+        Arrays.sort(words, Comparator.comparingInt(String::length));
+        for(int i=0;i<words.length;i++)
+        {
+            for(int j=0;j<words.length;j++)
+            {
+                if(i==j) continue;
+
+                if(words[j].contains(words[i]))
+                {
+                    res.add(words[i]);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
