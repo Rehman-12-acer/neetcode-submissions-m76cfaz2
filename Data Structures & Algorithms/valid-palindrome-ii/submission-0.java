093937d2d47e57b6700bcdc147f851c9b10afde0
Rehman-12-acer <rehmanacer999@gmail.com>
2026-09-23 23:38:28 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        
+        int l=0,r=s.length()-1;
+        while(l<r)
+        {
+            if(s.charAt(l)!=s.charAt(r))
+            {
+                return palindrome(l+1,r,s)||palindrome(l,r-1,s);
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+    private boolean palindrome(int l,int r, String s)
+    {
+
+        while(l<r)
+        {
+            if(s.charAt(l)!=s.charAt(r))
+            {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
